d2b95c8d06483ca96fccfd822fcf88627fd47bcc
Cef0PT <pascal.toelle@yahoo.de>
2025-02-04 14:50:59 +0100

    initialize new famgpytorch package

---
+++ famgpytorch/kernels/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from .approx_rbf_kernel import ApproxRBFKernel
+
+__all__ = [
+    "ApproxRBFKernel"
+]
---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+VERSION = "0.0.1"
+
+readme = open("README.md").read()
+
+setup(
+    name="famgpytorch",
+    version=VERSION,
+    description="An implementation of fast approximate Gaussian Processes in Pytorch utilizing GPyTorch",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Pascal Tölle",
+    project_urls= {"Source": "https://github.com/Cef0PT/Studienarbeit_Pascal_Toelle_Code"},
+    classifiers=["Development Status :: 3 - Alpha","Programming Language :: Python :: 3"],
+    packages=find_packages(),
+    install_requires=["gpytorch"]
+)
