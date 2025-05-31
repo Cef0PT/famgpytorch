858c78c95a5ece386fc9b32687cb614dd7709cfc
Cef0PT <pascal.toelle@yahoo.de>
2025-05-31 14:50:06 +0200

    preparation for new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import find_packages, setup
 
-VERSION = "0.0.1"
+VERSION = "0.1.0"
 
 readme = open("README.md").read()
 
@@ -12,10 +12,10 @@ setup(
     description="An implementation of fast approximate Gaussian Processes utilizing GPyTorch",
     long_description=readme,
     long_description_content_type="text/markdown",
-    author="Pascal Tölle",
-    project_urls= {"Source": "https://github.com/Cef0PT/Studienarbeit_Pascal_Toelle_Code"},
+    author="Pascal Alexander Tölle",
+    project_urls= {"Source": "https://github.com/Cef0PT/famgpytorch"},
     classifiers=["Development Status :: 3 - Alpha", "Programming Language :: Python :: 3"],
     packages=find_packages(),
-    install_requires=["torch", "gpytorch", "linear_operator"],  # TODO: add required version number
+    install_requires=["torch", "gpytorch", "linear_operator"],
     extras_require={"playground": ["ipython", "jupyter", "matplotlib", "numpy"]},
 )
